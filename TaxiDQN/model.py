87e98ed698ad0c51333c9f82861c570ec1b4d440
arribass <55694226+arribass@users.noreply.github.com>
2022-07-06 00:03:20 +0200

    organization

--- TaxiDQN/model.py
+++ TaxiDQN/model.py
@@ -6,6 +6,10 @@ class DQN(nn.Module):
 
     def __init__(self, outputs):
         super(DQN, self).__init__()
-
+        self.linear1 = torch.nn.Linear(100, 200)
+        self.activation = torch.nn.ReLU()
+        self.linear2 = torch.nn.Linear(200, 10)
+        self.softmax = torch.nn.Softmax()
+        
     def forward(self, x):
         return x
