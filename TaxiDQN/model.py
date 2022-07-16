ee5dd9fdd678d1f41f63e01e464a1c02be42c184
arribass <55694226+arribass@users.noreply.github.com>
2022-07-16 23:48:13 +0200

    avances en DQN Taxi

--- TaxiDQN/model.py
+++ TaxiDQN/model.py
@@ -1,15 +1,24 @@
+"""
+    Modelo DQN - Paquete TaxiDQN
+    Adrián Arribas
+    UPNA 
+"""
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class DQN(nn.Module):
-
+    ''' Implementacion del modelo DQN '''
     def __init__(self, outputs):
         super(DQN, self).__init__()
-        self.linear1 = torch.nn.Linear(100, 200)
-        self.activation = torch.nn.ReLU()
-        self.linear2 = torch.nn.Linear(200, 10)
-        self.softmax = torch.nn.Softmax()
-        
+        self.linear1 = nn.Linear(100, 200)
+        self.activation = nn.ReLU()
+        self.linear2 = nn.Linear(200, 10)
+        self.softmax = nn.Softmax()
+
     def forward(self, x):
+        x = self.linear1(x)
+        x = self.activation(x)
+        x = self.linear2(x)
+        x = self.softmax(x)
         return x
